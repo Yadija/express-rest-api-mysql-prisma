7c14edc572083a386468ea73d2a6455090c4a179
Yadija <ayuliadi01@gmail.com>
2023-07-12 15:38:08 +0800

    implement error handling middleware

--- src/application/web.js
+++ src/application/web.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import errorMiddleware from '../middleware/errorMiddleware.js';
 
 export const web = express();
 web.use(express.json());
+
+web.use(errorMiddleware);
